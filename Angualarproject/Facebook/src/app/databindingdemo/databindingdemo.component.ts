f1ebbf79a232c00705ac45a8222be6510dbd0c56
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-12-05 20:56:27 +0530

    example

--- Angualarproject/Facebook/src/app/databindingdemo/databindingdemo.component.ts
+++ Angualarproject/Facebook/src/app/databindingdemo/databindingdemo.component.ts
@@ -14,5 +14,32 @@ export class DatabindingdemoComponent {
   abc(){
     console.log("HI Abc Function ...!")
   }
-  
+
+  count=0;
+  increment()
+  {
+    this.count++;
+    console.log(this.count)
+  }
+  decrement()
+  {
+    this.count--;
+    console.log(this.count)
+  }
+  myname(name:string){
+    console.log("Hi Mr !"+name)
+  }
+
+  total=0;
+  emi=0;
+  caculate(pri:any,time:any, roi:any){
+    // console.log(pri);
+    // console.log(time);
+    // console.log(roi);
+
+    this.total = (pri *  time  * roi)/100; 
+    this.emi=this.total/time;
+  }
 }
+
+
