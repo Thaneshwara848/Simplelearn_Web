c56d086ee47c7701396da5d2879e764f591443a1
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-12-05 22:04:46 +0530

    For loop

--- Angualarproject/Facebook/src/app/directivedemo/directivedemo.component.ts
+++ Angualarproject/Facebook/src/app/directivedemo/directivedemo.component.ts
@@ -10,4 +10,10 @@ export class DirectivedemoComponent {
   abc:boolean= true;
   xyz:boolean= false;
 
+fruits=["Apple","Banana","Chicko","Dates","Graps","PiApple"];
+addfruit(myfruit:any)
+{
+  console.log(myfruit);
+  this.fruits.push(myfruit);
+}
 }
